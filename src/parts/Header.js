4d4e692fb29390b6a6e79ab4254a499a546a48ab
masegy <augyerislioga.s@gmail.com>
2021-09-20 22:28:21 +0700

    First Commit

---
+++ src/parts/Header.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Button from "elements/Button";
+import BrandIcon from "parts/IconText";
+import Fade from "react-reveal/Fade";
+
+export default function Header(props) {
+  const getNavLinkClass = (path) => {
+    return props.location.pathname === path ? " active" : "";
+  };
+
+  return (
+    <Fade>
+      <header className="spacing-sm">
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light">
+            <BrandIcon />
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav ml-auto">
+                <li className={`nav-item${getNavLinkClass("/")}`}>
+                  <Button className="nav-link" type="link" href="/">
+                    Beranda
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/book-nomor-antrian")}`}>
+                  <Button className="nav-link" type="link" href="/book-nomor-antrian">
+                    Book Nomor Antrian
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("daftar-bank")}`}>
+                  <Button className="nav-link" type="link" href="daftar-bank">
+                    Daftar Bank & Info Antrian
+                  </Button>
+                </li>
+                <li className={`nav-item`}>
+                  <Button className="btn ml-2"hasShadow isPrimary>Sign Out</Button>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </header>
+    </Fade>
+  );
+}
